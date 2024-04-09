defe7680a089d38efc3b6dc820f187dcef53debc
Ron-Rubin <48482141+Ron-Rubin@users.noreply.github.com>
2024-04-09 14:14:00 +0300

    Commit

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,15 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import "./MovieDetails.css";
 
 function MovieDetails({ movie, onFavoriteToggle, favorites }) {
 
   if (!movie) return null;
 
-  /*----  Setting Like button status up/down  ----*/
-  function likeToggle() {
-    var element = document.getElementById("likeButton");
-      element.classList.toggle("fa-thumbs-up"); 
-  }
+
   /*----  Handaling info button  ----*/
   function infoButton() {
     var element = document.getElementById("infoToggle");
@@ -23,12 +19,11 @@ function MovieDetails({ movie, onFavoriteToggle, favorites }) {
       <h2 className="movieEpisode">Episode : {movie.episode_id}</h2>
       <h3 className="movieYear">Realese Date:&nbsp;{movie.release_date}</h3>
       <h3 className="movieProducer">By:&nbsp; {movie.producer}</h3>
-      <button className="likeBtn" onClick={() =>{onFavoriteToggle(movie); likeToggle()}}>
+      <button className="likeBtn" onClick={() =>{onFavoriteToggle(movie);}}>
         <div>
           { favorites.some(fav => fav.episode_id === movie.episode_id) ? "Like" : "Liked"} 
         </div>
       </button>
-      <div id={"likeButton"} className="fa likeButtonChanger" />{/* --Like button-- */}
         <div className="infoBox">
           <p id="infoToggle" className="infoToggle-off"> {movie.opening_crawl} </p>
         </div>
